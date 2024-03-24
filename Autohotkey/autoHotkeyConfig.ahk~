72a12f66273ac69822106eef93232c8b067dce59
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-03-24 11:54:39 +0900

    Update to Dir

---
+++ Autohotkey/autoHotkeyConfig.ahk~
@@ -0,0 +1,14 @@
+lt+A를 Ctrl+A로 리맵핑
+!a::
+Send, ^a
+return
+
+; Alt+C를 Ctrl+C로 리맵핑
+!c::
+Send, ^c
+return
+
+; Alt+V를 Ctrl+V로 리맵핑
+!v::
+Send, ^v
+return
---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+#C:\Users\<USER>\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
+#위 디렉토리에 붙여넣으시오
+
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/clean-detailed.omp.json' | Invoke-Expression
+Set-Alias open explorer
