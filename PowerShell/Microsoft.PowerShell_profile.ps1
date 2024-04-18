959efeea235b796be9306d8ca761e61ca4963d42
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-04-18 17:19:02 +0900

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
-#C:\Users\<USER>\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
-#위 디렉토리에 붙여넣으시오
+# C:\Users\<USER>\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 
+# 위 디렉토리에 붙여넣으시오
+# 혹은 터미널에서 vim $profile 을 직접 편집하시오
 
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/clean-detailed.omp.json' | Invoke-Expression
 Set-Alias open explorer
