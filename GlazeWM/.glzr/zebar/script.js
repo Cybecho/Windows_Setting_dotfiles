1be990d449cdf554c8cb7a7e9006a996f42621ea
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-10-01 11:11:55 +0900

    delete to zebar setting files

--- GlazeWM/.glzr/zebar/script.js
+++
@@ -1,5 +0,0 @@
-export function focusWorkspace(event, context) {
-  console.log('Focus button clicked!', event, context);
-  const id = event.target.id;
-  context.providers.glazewm.focusWorkspace(id);
-}
